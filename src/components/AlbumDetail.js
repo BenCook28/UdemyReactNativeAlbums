d9fe7d461c1ded7913720e4208179d9c6889e415
Benjamin Cook <benjamincook@benjamins-mbp.home>
2018-05-28 12:20:41 -0400

    added headerTextStyle

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,19 +5,24 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image } = album;
-    const { headerContentStyle, thumbnailStyle } = styles;
+    const {
+        headerContentStyle,
+        thumbnailStyle,
+        thumbnailContainerStyle,
+        headerTextStyle
+    } = styles;
 
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image
                         source={{ uri: thumbnail_image }}
                         style={thumbnailStyle}
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
@@ -33,6 +38,15 @@ const styles = {
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItem: 'center',
+        marginLeft: 10,
+        marginRight: 10
     }
 }
 
